bf6601f31c07da1ead29a16f715fe111bf08d204
sandeep-raj-saimon <36258508+sandeep-raj-saimon@users.noreply.github.com>
2026-06-14 13:29:38 +0530

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        m = {}
+        for index,num in enumerate(nums):
+            m[num] = index
+        print(m)
+        for index, num in enumerate(nums):
+            diff = target - num
+            if m.get(diff) is not None and m.get(diff) != index:
+                return [index, m.get(diff)]
+        return False
+            
