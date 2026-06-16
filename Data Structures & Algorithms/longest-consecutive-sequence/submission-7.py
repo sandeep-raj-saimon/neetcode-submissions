9c0094886bc6635e6709e30896ff60e78b83dc39
sandeep-raj-saimon <36258508+sandeep-raj-saimon@users.noreply.github.com>
2026-06-16 18:28:25 +0530

    Add: longest-consecutive-sequence - submission-7

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-7.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        m = {}
+        for index, num in enumerate(nums):
+            m[num] = index
+
+        count = 0
+        max_count = float('-inf')
+
+        for num in nums:
+            while m.get(num) is not None:
+                num -=1
+                count+=1
+                max_count = max(max_count, count)
+            count=0
+        return max_count if max_count != float('-inf') else 0
