3afecc45530be05d2479b410cb2c5b54438e053b
sandeep-raj-saimon <36258508+sandeep-raj-saimon@users.noreply.github.com>
2026-06-23 00:03:43 +0530

    Add: sliding-window-maximum - submission-6

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-6.py
@@ -0,0 +1,22 @@
+import heapq
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        q = []
+        result = []
+        # 1,2,1,4,1,2,1
+        # 0,1,2,3,4,5,6
+        # k = 3
+        """
+        when i is at 2, we need to start finding the max
+        starting element of heap is the max
+        lets say i is at 2
+        max will be 4, present at 4
+        """
+        for i, num in enumerate(nums):
+            heapq.heappush(q, (-num, i))
+
+            if i >= k-1:
+                while q[0][1] <= i-k:
+                    heapq.heappop(q)
+                result.append(-q[0][0])
+        return result
