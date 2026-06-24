ad7cbdc7519facbe572ac8ee1fa3f0c48099835e
sandeep-raj-saimon <36258508+sandeep-raj-saimon@users.noreply.github.com>
2026-06-24 16:17:20 +0530

    Add: largest-rectangle-in-histogram - submission-5

---
+++ Data Structures & Algorithms/largest-rectangle-in-histogram/submission-5.py
@@ -0,0 +1,27 @@
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        # we need to store how many elements are consecutively bigger
+        # on left and right of the current element
+        n = len(heights)
+        left = [0]*n
+        right = [0]*n
+        stack = []
+
+        for i,height in enumerate(heights):
+            while stack and height <= stack[-1][0]:
+                e = stack.pop()
+                left[i] += left[e[1]] + 1
+            stack.append([height, i])
+        stack = []
+        for i, height in reversed(list(enumerate(heights))):
+            while stack and height <= stack[-1][0]:
+                e = stack.pop()
+                right[i] += right[e[1]] + 1
+            stack.append([height, i])
+        print(left)
+        print(right)
+
+        ans = float('-inf')
+        for i in range(n):
+            ans = max(ans, (left[i] + right[i] + 1)* heights[i])
+        return ans
